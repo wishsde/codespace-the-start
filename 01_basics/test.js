c0e72e9468c5d459cfeeab31090e5daa542f4638
Vishal Waghmare <163580488+wishsde@users.noreply.github.com>
2025-02-02 06:35:32 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,1 @@
+console.log("Dr.Babasaheb Ambedkar");
---
+++ 01_basics/variable.js
@@ -0,0 +1,27 @@
+// assign the memory in js we use the const, let. 
+// var is also the memory assign but this is abort , prefer not to use 
+
+// Every line of code is analysis by Node.js 
+
+const accountId= 123
+let accountEmailId="vishal@google.com";
+let accountPassword="1235";
+let accountCity="Nagpur";
+
+/* here - their is no mandatory to use semicolon ";"
+we declare the variable without let also.*/
+
+// How to run this code 
+
+console.log(accountId);
+// if we ("accountId") then it print "accountId"
+
+// if we call all in one time in one time and in tabular from 
+
+console.table([accountId,accountEmailId,accountPassword, accountCity]);
+
+
+
+
+
+
